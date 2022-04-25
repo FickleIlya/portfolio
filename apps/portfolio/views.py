3a407ff0e334f9ee4600b0a2658dbcb2ba1a9848
FIckleIlya <fickle041@gmail.com>
2022-04-25 15:15:24 +0300

    add tempaltes, media, admin

--- apps/portfolio/views.py
+++ apps/portfolio/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
+def homepage(request):
+    context = {}
+    return render(request, 'portfolio_templates/homepage.html', context=context)
