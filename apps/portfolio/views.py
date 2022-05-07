b4d1eb3854f9e9988883677e1d28e2b855f183e7
FIckleIlya <fickle041@gmail.com>
2022-05-07 20:23:58 +0300

    change static and media files

--- apps/portfolio/views.py
+++ apps/portfolio/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from .models import Project
+import datetime as dt
 
 
 # Create your views here.
 def homepage(request):
     projects = Project.objects.all()
+    today = dt.date.today()
+    if today.day >= 4 and today.month >= 1:
+        my_age = today.year % 1000
+    else:
+        my_age = (today.year % 1000) - 1
     context = {
+        'my_age': my_age,
         'projects': projects,
     }
     return render(request, 'portfolio_templates/homepage.html', context=context)
